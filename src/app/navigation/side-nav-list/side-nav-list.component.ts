f96840a150616b76f7e6a398a1cf66cb791a633f
ahmed-abdelfattah <ahmed-abdelfattah@bitbucket.org>
2020-04-15 10:47:05 +0200

    adding sidenav and header links

--- src/app/navigation/side-nav-list/side-nav-list.component.ts
+++ src/app/navigation/side-nav-list/side-nav-list.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-side-nav-list',
   templateUrl: './side-nav-list.component.html',
   styleUrls: ['./side-nav-list.component.css']
 })
-export class SideNavListComponent implements OnInit {
+export class SideNavListComponent implements OnInit ,OnDestroy{
   @Output('onCloseSidenav') onCloseSidenav = new EventEmitter<void>()
-
-  constructor() { }
+  isAuth: boolean=false;
+  authSubscription:Subscription
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.authSubscription =  this.authService.authChange.subscribe(authStatus=>{
+      this.isAuth=authStatus;
+    });
   }
   onClose() {
     this.onCloseSidenav.emit();
+    this.logout()
+  }
+
+  logout(){
+    this.authService.logout()
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 }

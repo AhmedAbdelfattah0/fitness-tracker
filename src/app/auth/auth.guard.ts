d885fdc137831dd0d14cf5029df3ab117c0c6ca3
ahmed-abdelfattah <ahmed-abdelfattah@bitbucket.org>
2020-04-21 13:45:01 +0200

    adding canLoad to AuthGard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad ,Route} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-
+ 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService:AuthService, private router:Router){}
+  
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
@@ -17,4 +18,10 @@ export class AuthGuard implements CanActivate {
     return this.router.navigate(['login'])
   }
   
+
+  canLoad( route: Route,) {
+    if( this.authService.isAuth())
+    return true;
+    else
+    return this.router.navigate(['login'])  }
 }
